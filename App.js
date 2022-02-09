7b5dc9aee2c5d46a7be0ee7dc843b0305254a6fe
lucas <luqui31@gmail.com>
2022-02-09 10:45:02 +0100

    movie rating app

--- App.js
+++ App.js
@@ -1,20 +1,118 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { Button, StyleSheet, Text, TextInput, View, StatusBar, FlatList, ScrollView } from 'react-native';
+import {
+    NavigationContainer,
+    useFocusEffect,
+    useNavigation,
+    useRoute,
+  } from "@react-navigation/native";
+  import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+
+    const addMovie = (movie) => {
+        setMovieList([...movieList, {
+            id: movieList.length,
+            movie: {
+                title: movie.title,
+                note: note,
+                desc: desc
+            }
+        }])
+    }
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                name="Home"
+                component={MovieList}
+                initialParams={{ addMovie: null }}
+                />
+                <Stack.Screen name="Add" component={AddMovie} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const MovieList = () => {
+    const navigation = useNavigation();
+    const route = useRoute();
+    const [movieList, setMovieList] = useState([]);
+
+    const addMovie = (movie) => {
+        setMovieList((current) => [...current, { id: current.length, movie: movie }]);
+    };
+    
+    useFocusEffect(() => {
+        if (!route.params.addMovie) return;
+        addMovie(route.params.addMovie);
+        route.params.addMovie = null;
+    });
+
+    return (
+        <View>
+            <Text>Movies List</Text>
+            <Button
+                title="Ajouter un film"
+                onPress={() => {
+                    navigation.navigate("Add");
+                }}
+            />
+            <FlatList
+                data={movieList}
+                keyExtractor={(item) => item.id}
+                renderItem={({item}) => <Movie title={item.movie.title} note={item.movie.note} desc={item.movie.desc}></Movie>}
+            ></FlatList>
+        </View>
+    );
+}
+
+const Movie = ({title, note, desc}) => {
+    return (
+        <View>
+            <Text>{title}</Text>
+            <Text>{note}/5</Text>
+            <Text>{desc}</Text>
+        </View>
+    );
+}
+
+const AddMovie = () => {
+    const navigation = useNavigation();
+
+    const [valid, setValid] = useState(false);
+    const [img, setImg] = useState("");
+    const [title, setTitle] = useState("");
+    const [note, setNote] = useState("");
+    const [desc, setDesc] = useState("");
+
+    useEffect(() => {
+        if (title.length >= 2 && +note && note.length == 1 && desc.length >= 10)
+            setValid(true);
+    }, [title, note, desc]);
+
+    const checkNote = (text) => {
+        if ((+text || text == "") && (parseInt(text) <= 5 || text.length == 0))
+            setNote(text);
+    }
+
+    return (
+        <View>
+            <Text>Movie Rating</Text>
+            <View>
+                <TextInput value={title} onChangeText={setTitle} placeholder='Titre du film'></TextInput>
+                <TextInput value={note} onChangeText={checkNote} keyboardType='numeric' placeholder='Note /5'></TextInput>
+                <TextInput value={desc} onChangeText={setDesc} placeholder='Description'></TextInput>
+                <Button title='Ajouter' disabled={!valid} onPress={() => {
+                    navigation.navigate("Home", { addMovie: {title: title, note: note, desc: desc} })
+                }}></Button>
+            </View>
+            <StatusBar style='auto'></StatusBar>
+        </View>
+    );
+}
+
+export default App;
